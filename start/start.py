4609de68acc00f132618be5a298676d33d273581
Magiclu550 <843983728@qq.com>
2019-06-07 12:28:04 +0800

    py：jsmod2-start copyright

--- start/start.py
+++ start/start.py
@@ -1,9 +1,16 @@
+# coding=utf-8
 """
+
+Jsmod2 is a java-based scpsl server initiated by jsmod2.cn.
+It needs to rely on smod2 and proxy. jsmod2 is an open source
+free plugin that is released under the GNU license. Please read
+the GNU open source license before using the software. To understand
+the appropriateness, if infringement, will be handled in accordance
+with the law, @Copyright Jsmod2 China,more can see <a href="http://jsmod2.cn">that<a>
+
 这个python脚本可以用于下载java和scala相关库
 并同时启动服务端
 
 使用bat和shell脚本可以安装python的库
 
 """
-
-
