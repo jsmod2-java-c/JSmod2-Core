62cf61cb0b3ccddda263308ba50e391e621b7060
magiclu550 <843983728@qq.com>
2019-08-16 09:51:26 +0800

    [JSMOD@2_470_COMMIT] China No1

--- JSMod2Protocol/src/main/java/cn/jsmod2/network/protocol/map/map/Get079InteractionRoomsPacket.java
+++ JSMod2Protocol/src/main/java/cn/jsmod2/network/protocol/map/map/Get079InteractionRoomsPacket.java
@@ -1,4 +1,21 @@
 package cn.jsmod2.network.protocol.map.map;
 
-public class Get079InteractionRoomsPacket {
+import cn.jsmod2.api.map.Room;
+import cn.jsmod2.api.map.Scp079InteractionType;
+import cn.jsmod2.core.protocol.GetPacket;
+
+public class Get079InteractionRoomsPacket extends GetPacket {
+
+    public static final int ID = 151;
+
+    public Scp079InteractionType type;
+
+    public Get079InteractionRoomsPacket() {
+        super(ID, Room.class);
+    }
+
+    @Override
+    public Room[] send() {
+        return (Room[]) requester.with("type",type).get().getProtocolArray(false).toArray();
+    }
 }
--- JSMod2Protocol/src/main/java/cn/jsmod2/network/protocol/map/map/GetBlastDoorPointsPacket.java
+++ JSMod2Protocol/src/main/java/cn/jsmod2/network/protocol/map/map/GetBlastDoorPointsPacket.java
@@ -1,4 +1,21 @@
 package cn.jsmod2.network.protocol.map.map;
 
-public class GetBlastDoorPointsPacket {
+import cn.jsmod2.api.team.Role;
+import cn.jsmod2.core.math.Vector;
+import cn.jsmod2.core.protocol.GetPacket;
+
+public class GetBlastDoorPointsPacket extends GetPacket {
+
+    public Role role;
+
+    public static final int ID = 152;
+
+    public GetBlastDoorPointsPacket(){
+        super(ID, Vector.class);
+    }
+
+    @Override
+    public Object send() {
+        return requester.with("role",role);
+    }
 }
