722baceb25ede06b200422d33870c037ad502f8d
magiclu550 <843983728@qq.com>
2019-08-27 13:32:27 +0800

    [commit] #1061 fix a bug

--- JPLS/src/main/java/cn/jsmod2/RPCHandler.java
+++ JPLS/src/main/java/cn/jsmod2/RPCHandler.java
@@ -1,17 +1,20 @@
 package cn.jsmod2;
 
-import cn.jsmod2.core.Console;
 import cn.jsmod2.core.Server;
 import cn.jsmod2.core.log.ServerLogger;
 import cn.jsmod2.core.plugin.PluginClassLoader;
 import cn.jsmod2.core.utils.Utils;
+import com.google.common.io.Resources;
 import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarException;
+
+import java.io.File;
+
 public class RPCHandler{
 
-    private Sigar sigar = new Sigar();
+    private Sigar sigar = initSigar();
 
-    public void start(String sw){
+    public String start(String sw){
         sw = sw.replace("1","-w")
                 .replace("2","-u")
                 .replace("3","-lr")
@@ -19,7 +22,9 @@ public class RPCHandler{
                 .replace("5","-github")
                 .replace("6","-n")
                 .replace("7","-a");
-        ServerStarter.getInstance().startNow(new String[]{sw});
+        final String args = sw;
+        new Thread(()->ServerStarter.getInstance().startNow(new String[]{args})).start();
+        return "server has started";
     }
 
     public String get_status(){
@@ -27,8 +32,12 @@ public class RPCHandler{
         return "started";
     }
 
-    public void execute(String command){
-        Console.getConsole().runConsoleCommandWithEmerald(command);
+
+    public String execute(String command){
+        if(Server.getSender()==null){
+            return "the server has stopped";
+        }
+        return Server.getSender().getServer().getPluginManager().consoleExecuteCommand(command)+"";
     }
 
     public double cpu() {
@@ -49,12 +58,35 @@ public class RPCHandler{
     }
 
 
-    public void stop(){
+    public boolean stop(){
         if(Server.getSender() == null){
             ServerLogger.getLogger().multiWarn(getClass(),"the server has stopped","","");
-            return;
+            return false;
         }
         Server.getSender().getServer().close();
+        return true;
     }
+
+    private Sigar initSigar() {
+        String os = System.getProperty("os.name");
+        try {
+            String file = Resources.getResource("sigar/.sigar_shellrc").getFile();
+            File classPath = new File(file).getParentFile();
+
+            String path = System.getProperty("java.library.path");
+            if (os.toLowerCase().contains("win")) {
+                path += ";" + classPath.getCanonicalPath();
+            } else {
+                path += ":" + classPath.getCanonicalPath();
+            }
+            System.setProperty("java.library.path", path);
+
+            return new Sigar();
+        } catch (Exception e) {
+            Utils.printException(e);
+            return null;
+        }
+    }
+
 
 }
--- JPLS/src/main/java/cn/jsmod2/ServerStarter.java
+++ JPLS/src/main/java/cn/jsmod2/ServerStarter.java
@@ -7,18 +7,22 @@ import cn.jsmod2.core.plugin.SpringContextUtil;
 import cn.jsmod2.core.utils.Utils;
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.server.PropertyHandlerMapping;
+import org.apache.xmlrpc.server.XmlRpcHandlerMapping;
 import org.apache.xmlrpc.server.XmlRpcServerConfigImpl;
 import org.apache.xmlrpc.webserver.WebServer;
 
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 import static cn.jsmod2.core.utils.Utils.contains;
 
 /**
  * -w -u -lr -lm -github -n -a
- * -w 打开web
+ *  * -w 打开web
  * -u 打开ui
  * -lr 打开round的log监听
  * -lm 打开multiAdmin和游戏的log监听
@@ -31,6 +35,7 @@ public class ServerStarter {
 
     private static ServerStarter instance;
 
+    public static final String P_KEY = "jsmod";
     //Failed to instantiate class cn.jsmod2.ServerStarter$RPCHandler
     public void start(String[] args){
 
@@ -45,11 +50,27 @@ public class ServerStarter {
                 ServerLogger.getLogger().multiInfo(getClass(),"xmlrpc is started...","","");
                 WebServer webServer = new WebServer(port);
                 PropertyHandlerMapping mapping = new PropertyHandlerMapping();
-                mapping.addHandler("jsmod", RPCHandler.class);
+                mapping.addHandler(P_KEY, RPCHandler.class);
                 webServer.getXmlRpcServer().setHandlerMapping(mapping);
                 XmlRpcServerConfigImpl serverConfig = (XmlRpcServerConfigImpl)webServer.getXmlRpcServer().getConfig();
                 serverConfig.setEnabledForExceptions(true);
                 serverConfig.setContentLengthOptional(false);
+                //
+                try {
+                    Method[] methods = RPCHandler.class.getDeclaredMethods();
+                    XmlRpcHandlerMapping mapping1 = webServer.getXmlRpcServer().getHandlerMapping();
+                    Field field = mapping1.getClass().getSuperclass().getDeclaredField("handlerMap");
+                    field.setAccessible(true);
+                    Method getNewMethod = mapping1.getClass().getSuperclass().getDeclaredMethod("newXmlRpcHandler",Class.class,Method[].class);
+                    getNewMethod.setAccessible(true);
+                    Map map = (Map) field.get(mapping1);
+                    for(Method method:methods) {
+                        map.put(P_KEY+"."+method.getName(),getNewMethod.invoke(mapping1,new Object[]{RPCHandler.class,new Method[]{method}}));
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                //
                 webServer.start();
             }catch (IOException|XmlRpcException e){
                 Utils.printException(e);
